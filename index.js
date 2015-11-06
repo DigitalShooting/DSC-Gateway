b2baea98e6db4bcd7991afda320fb72089fa906b
Jannik Lorenz <dev@janniklorenz.de>
2015-11-06 20:12:06 +0100

    [FIX] Clean up

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ var http = require("http")
 var config = require("./config/")
 var SocketClient = require("socket.io-client")
 
+
+
+// -------- Server Socket --------
 var app = express({ strict: true })
-
-
-
-// Set up express & socket.io
 var server = http.Server(app)
 var io = require('socket.io')(server)
 server.listen(config.network.port, config.network.address)
@@ -15,12 +14,11 @@ server.on('listening', function() {
 	console.log('Express server started on at %s:%s', server.address().address, server.address().port)
 })
 
-
-
-// Server Socket
 io.on('connection', function(socket){
+	// send online DSCs to new connected client
 	sendOnlineLines(socket)
 
+	// triggers any given event on DSC
 	socket.on("setLine", function(data){
 		var lineSocket = config.lines[data.line].socket
 		if (lineSocket != undefined){
@@ -31,34 +29,41 @@ io.on('connection', function(socket){
 
 
 
+// -------- Line Sockets --------
+
+// store bool for each line id (true = online)
 var linesOnline = {}
 
-// Line Sockets
 for (id in config.lines){
 	registerLine(id)
 }
+
+// Register line with id
 function registerLine(id){
 	var line = config.lines[id]
 	linesOnline[id] = false
 
 	config.lines[id].socket = SocketClient("http://"+line.ip+":"+line.port)
 
+	// redirect following methods
 	var methods = ["setSession", "setConfig", "setData"]
 	for (i in methods){
 		setUpEvent(config.lines[id].socket, methods[i])
 	}
-
-
+	setUpConnection(config.lines[id].socket)
 
 	// set up socket event method
 	function setUpEvent(socket, method){
 		socket.on(method, function(data){
-			io.emit("setSession", {
+			io.emit(method, {
 				line: line._id,
 				data: data,
 			})
 		})
+	}
 
+	// set up connection methods
+	function setUpConnection(socket){
 		socket.on("connect", function(){
 			linesOnline[id] = true
 			sendOnlineLines(io)
@@ -69,8 +74,6 @@ function registerLine(id){
 		})
 	}
 }
-
-
 
 // send online lines event to socket or broadcast to all clients
 function sendOnlineLines(socket){
